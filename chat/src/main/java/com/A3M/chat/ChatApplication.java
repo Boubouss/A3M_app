f38959c3d587099dfa048a9ef27cd26fc938606f
Boubouss <redwanbsm@gmail.com>
2025-10-21 12:01:26 +0200

    Mise en place du projet

---
+++ chat/src/main/java/com/A3M/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.A3M.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
---
+++ licences/src/test/java/com/A3M/licences/LicencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.A3M.licences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
